c2ac50ea6283e9be74334036604928938f2e4b72
David <dstarry@ucm.es>
2024-02-06 21:59:55 +0100

    PRUEBA

--- src/main/java/com/ucm/gg/control/RootController.java
+++ src/main/java/com/ucm/gg/control/RootController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller 
 public class RootController {
 
-    @GetMapping("/")
+    @GetMapping("/front")
     public String front() { 
         return "front"; 
     }
