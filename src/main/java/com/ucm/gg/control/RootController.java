ea3feecc9b4f52bf250f08ccfbb2eac75e2b0a7a
DavidSG <98974095+DavidSG@users.noreply.github.com>
2024-02-06 21:54:00 +0100

    Add files via upload

---
+++ src/main/java/com/ucm/gg/control/RootController.java
@@ -0,0 +1,18 @@
+package com.ucm.gg.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller 
+public class RootController {
+
+    @GetMapping("/")
+    public String front() { 
+        return "front"; 
+    }
+
+    @GetMapping("/matchHistory")
+    public String matchHistory() { 
+        return "matchHistory"; 
+    }
+}
