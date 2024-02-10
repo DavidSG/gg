0c244daaa8c7e6f93da3fc90190e66c9aedf5634
Lizzen <daviddfdiaz@gmail.com>
2024-02-10 15:57:27 +0100

    arreglo mapeo campeones, main y ajustes

--- src/main/java/com/ucm/gg/control/RootController.java
+++ src/main/java/com/ucm/gg/control/RootController.java
@@ -11,6 +11,21 @@ public class RootController {
         return "main"; 
     }
 
+    @GetMapping("/campeones.html")
+    public String campeones() { 
+        return "campeones"; 
+    }
+
+    @GetMapping("/guides.html")
+    public String guides() { 
+        return "guides"; 
+    }
+
+    @GetMapping("/items.html")
+    public String items() { 
+        return "items"; 
+    }
+
     @GetMapping("/ajustes.html")
     public String ajustes() { 
         return "ajustes"; 
