566f176164b0430667b3a1eb6aa68f67dcdce4ef
David <dstarry@ucm.es>
2024-02-20 14:48:35 +0100

    cambios profesor con la plantilla

---
+++ src/main/java/es/ucm/fdi/iw/controller/RootController.java
@@ -0,0 +1,63 @@
+package es.ucm.fdi.iw.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * Non-authenticated requests only.
+ */
+@Controller
+public class RootController {
+
+    private static final Logger log = LogManager.getLogger(RootController.class);
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "old_index";
+    }
+
+    /*
+     * @GetMapping("/")
+     * public String index(Model model) {
+     * return "index";
+     * }
+     */
+
+    @GetMapping("/header")
+    public String header() {
+        return "header";
+    }
+
+    @GetMapping("/campeones")
+    public String campeones() {
+        return "campeones";
+    }
+
+    @GetMapping("/guides")
+    public String guides() {
+        return "guides";
+    }
+
+    @GetMapping("/items")
+    public String items() {
+        return "items";
+    }
+
+    @GetMapping("/miperfil")
+    public String miperfil() {
+        return "miperfil";
+    }
+
+    @GetMapping("/matchHistory")
+    public String matchHistory() {
+        return "matchHistory";
+    }
+}
