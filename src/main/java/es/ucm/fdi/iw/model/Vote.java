063d48880570c0ade152f7507efd3fb512dceb56
Lizzen <dferre07@ucm.com>
2024-04-30 14:53:00 +0200

    comit clase

--- src/main/java/es/ucm/fdi/iw/model/Vote.java
+++ src/main/java/es/ucm/fdi/iw/model/Vote.java
@@ -5,52 +5,25 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames={"guia", "autor"}))
+@Data
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = { "guia_id", "autor_id" }))
 public class Vote {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private Boolean vote;
-    
-    @Column(nullable = false)
-    private String autor;
-    
-    @Column(nullable = false)
-    private Long guia;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Boolean getVote() {
-        return vote;
-    }
-
-    public void setLike(Boolean vote){
-        this.vote = vote;
-    }
-
-    public String getAutor(){
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public Long getGuia() {
-        return guia;
-    }
-
-    public void setGuia(Long guia) {
-        this.guia = guia;
-    }
+    @ManyToOne
+    private User autor;
+    @ManyToOne
+    private Guia guia;
 }
