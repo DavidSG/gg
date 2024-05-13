b80fc27a49f52d9df3f68904274ae0230816ea5f
Lizzen <daviddfdiaz@gmail.com>
2024-05-13 15:35:38 +0200

    añadir y eliminar usuarios

--- src/main/java/es/ucm/fdi/iw/controller/AdminController.java
+++ src/main/java/es/ucm/fdi/iw/controller/AdminController.java
@@ -1,9 +1,7 @@
 package es.ucm.fdi.iw.controller;
 
-import java.io.IOException;
-
 import javax.persistence.EntityManager;
-import javax.servlet.http.HttpServletResponse;
+
 import javax.servlet.http.HttpSession;
 import javax.swing.JOptionPane;
 import javax.transaction.Transactional;
@@ -12,21 +10,17 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import es.ucm.fdi.iw.controller.UserController.NoEsTuPerfilException;
 import es.ucm.fdi.iw.model.User;
 import es.ucm.fdi.iw.model.User.Role;
 
@@ -56,7 +50,6 @@ public class AdminController {
     @ResponseBody
     public ResponseEntity<String> agregarUsuario(@RequestBody User target, HttpSession session) {
         // Aquí puedes realizar la lógica para agregar el usuario a la base de datos o a cualquier otro almacenamiento
-        log.info("target: {}", target);
         try{
             User requester = (User)session.getAttribute("u");
             if (requester.hasRole(Role.ADMIN)) {
@@ -73,5 +66,35 @@ public class AdminController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("Error al crear el usuario: " + e.getMessage());
         }
+    }
+    
+    @Transactional
+    @GetMapping("/{id}/eliminar")
+    public String eliminarUsuario(@PathVariable Long id, HttpSession session, Model model) {
+        User admin = (User) session.getAttribute("u");
+        if (id != admin.getId()){
+            User u = entityManager.find(User.class, id);
+            entityManager.createQuery(
+                "DELETE FROM Guia g WHERE g.autor = :usuario ")
+                .setParameter("usuario", u)
+                .executeUpdate();
+            
+            entityManager.createQuery(
+                "DELETE FROM Vote v WHERE v.autor = :usuario ")
+                .setParameter("usuario", u)
+                .executeUpdate();
+
+            entityManager.createQuery(
+                "DELETE FROM Comentario c WHERE c.autor = :usuario ")
+                .setParameter("usuario", u)
+                .executeUpdate();
+
+            entityManager.createQuery(
+                            "DELETE FROM User u WHERE u.id = :usuario ")
+                            .setParameter("usuario", id)
+                            .executeUpdate();
+        }
+
+        return "redirect:/listaUsers";
     }
 }
--- src/main/java/es/ucm/fdi/iw/controller/RootController.java
+++ src/main/java/es/ucm/fdi/iw/controller/RootController.java
@@ -11,10 +11,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import es.ucm.fdi.iw.model.Guia;
 import es.ucm.fdi.iw.model.Item;
+import es.ucm.fdi.iw.model.User;
 
 /**
  * Non-authenticated requests only.
@@ -107,6 +109,18 @@ public class RootController {
     @GetMapping("/admin")
     public String admin() {
         return "admin";
+    }
+
+    @GetMapping("/newUser")
+    public String newUser() {
+        return "newUser";
+    }
+
+    @GetMapping("/listaUsers")
+    public String listaUsers(Model model) {
+        List<User> userList = entityManager.createQuery("select u from User u", User.class).getResultList();
+        model.addAttribute("userList", userList);
+        return "listaUsers"; // Nombre de la vista Thymeleaf
     }
 
     @GetMapping("/matchHistory")
